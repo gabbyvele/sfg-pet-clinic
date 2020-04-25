70de94cad4bb45d85a94e473fdd0f14798db74b9
gabbyvele <gabbyvele@gmail.com>
2020-04-25 20:24:31 +0200

    Update Map Impl to manage setting of ID property Close #24

--- pet-clinic-web/src/main/java/com/jooce/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/jooce/sfgpetclinic/bootstrap/DataLoader.java
@@ -21,25 +21,23 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        addOwner(1L, "Gabriel", "Matshabe");
-        addOwner(2L, "Mika", "Zindagi");
+        addOwner("Gabriel", "Matshabe");
+        addOwner("Mika", "Zindagi");
 
-        addVet(1L, "Gabby", "Vele");
-        addVet(2L, "Jin", "Zindagi");
+        addVet("Gabby", "Vele");
+        addVet("Jin", "Zindagi");
     }
 
-    private void addOwner(long id, String firstName, String lastName) {
+    private void addOwner(String firstName, String lastName) {
         Owner owner = new Owner();
-        owner.setId(id);
         owner.setFirstName(firstName);
         owner.setLastName(lastName);
 
         ownerService.save(owner);
     }
 
-    private void addVet(long id, String firstName, String lastName) {
+    private void addVet(String firstName, String lastName) {
         Vet vet = new Vet();
-        vet.setId(id);
         vet.setFirstName(firstName);
         vet.setLastName(lastName);
 
