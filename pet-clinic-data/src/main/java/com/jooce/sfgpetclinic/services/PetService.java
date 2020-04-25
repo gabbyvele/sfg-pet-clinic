fc6235496f25ee663f2f72876fbb6c1379eb0280
gabbyvele <gabbyvele@gmail.com>
2020-04-25 14:44:14 +0200

    refactoring Crud

--- pet-clinic-data/src/main/java/com/jooce/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/jooce/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.jooce.sfgpetclinic.services;
 
 import com.jooce.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/jooce/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/jooce/sfgpetclinic/services/VetService.java
@@ -1,15 +1,7 @@
 package com.jooce.sfgpetclinic.services;
 
-import com.jooce.sfgpetclinic.model.Owner;
 import com.jooce.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Owner> findAll();
 }
