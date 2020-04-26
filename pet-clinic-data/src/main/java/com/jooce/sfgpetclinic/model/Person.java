ac1f84e34313ab8848251774811de8de471ebf0b
gabbyvele <gabbyvele@gmail.com>
2020-04-26 20:01:00 +0200

    Add Lombok Close #52

--- pet-clinic-data/src/main/java/com/jooce/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/jooce/sfgpetclinic/model/Person.java
@@ -1,6 +1,8 @@
 package com.jooce.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
@@ -8,6 +10,8 @@ import javax.persistence.MappedSuperclass;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
 
@@ -16,4 +20,10 @@ public class Person extends BaseEntity {
 
     @Column(name = "last_name")
     public String lastName;
+
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
--- pet-clinic-web/src/main/java/com/jooce/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/jooce/sfgpetclinic/bootstrap/DataLoader.java
@@ -29,7 +29,7 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         int countPets = petTypeService.findAll().size();
 
-        if (countPets == 0){
+        if (countPets == 0) {
             loadData();
         }
     }
