fb1658f38ee29a85d5570485bdef06ee6aeed11f
gabbyvele <gabbyvele@gmail.com>
2020-04-26 11:07:39 +0200

    Create pet type map impl closes #45

---
+++ pet-clinic-data/src/main/java/com/jooce/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.jooce.sfgpetclinic.services;
+
+import com.jooce.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/jooce/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package com.jooce.sfgpetclinic.services.map;
+
+import com.jooce.sfgpetclinic.model.PetType;
+import com.jooce.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findByID(Long id) {
+        return super.findById(id);
+    }
+}
