ec96f8f94ed4bf5e4ecbcc4dcff5074aa39eb256
gabbyvele <gabbyvele@gmail.com>
2020-04-26 12:31:39 +0200

    enhance owners with contact info and pets close #33

--- pet-clinic-web/src/main/java/com/jooce/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/jooce/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.jooce.sfgpetclinic.bootstrap;
 
 import com.jooce.sfgpetclinic.model.Owner;
+import com.jooce.sfgpetclinic.model.Pet;
 import com.jooce.sfgpetclinic.model.PetType;
 import com.jooce.sfgpetclinic.model.Vet;
 import com.jooce.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.jooce.sfgpetclinic.services.PetTypeService;
 import com.jooce.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -24,22 +27,32 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        addOwner("Gabriel", "Matshabe");
-        addOwner("Mika", "Zindagi");
+        PetType dog = addPetType("Dog");
+        PetType cat = addPetType("Cat");
+
+        Owner gabriel = addOwner("Gabriel", "Matshabe", "Roodeport", "Roodeport", "0832465689");
+        Pet gabsPet = addPet("Rosco", dog, gabriel, LocalDate.now());
+        gabriel.getPets().add(gabsPet);
+
+        Owner mika = addOwner("Mika", "Zindagi", "Bucleugh", "Bucleugh", "08432123491");
+        Pet mikasPet1 = addPet("Buddy", dog, mika, LocalDate.now());
+        mika.getPets().add(mikasPet1);
+        Pet mikasPet2 = addPet("Buddy", cat, mika, LocalDate.now());
+        mika.getPets().add(mikasPet2);
 
         addVet("Gabby", "Vele");
         addVet("Jin", "Zindagi");
-
-        addPetType("Dog");
-        addPetType("Cat");
     }
 
-    private void addOwner(String firstName, String lastName) {
+    private Owner addOwner(String firstName, String lastName, String address, String city, String telephone) {
         Owner owner = new Owner();
         owner.setFirstName(firstName);
         owner.setLastName(lastName);
+        owner.setAddress(address);
+        owner.setCity(city);
+        owner.setTelephone(telephone);
 
-        ownerService.save(owner);
+       return ownerService.save(owner);
     }
 
     private void addVet(String firstName, String lastName) {
@@ -55,5 +68,15 @@ public class DataLoader implements CommandLineRunner {
         petType.setName(name);
 
         return petTypeService.save(petType);
+    }
+
+    private Pet addPet(String name, PetType petType, Owner owner, LocalDate birthDate) {
+        Pet pet = new Pet();
+        pet.setName(name);
+        pet.setPetType(petType);
+        pet.setOwner(owner);
+        pet.setBirthDate(birthDate);
+
+        return pet;
     }
 }
