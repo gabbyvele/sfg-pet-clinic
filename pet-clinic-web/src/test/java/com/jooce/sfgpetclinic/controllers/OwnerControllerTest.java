0fa412d0b1cb4d94e38f9a98a86a5220ac5f5aea
gabbyvele <gabbyvele@gmail.com>
2020-04-27 14:55:49 +0200

    Write CRUD Tests for the Owner Spring Data JPA service #51

---
+++ pet-clinic-web/src/test/java/com/jooce/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,56 @@
+package com.jooce.sfgpetclinic.controllers;
+
+import com.jooce.sfgpetclinic.model.Owner;
+import com.jooce.sfgpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    @Mock
+    private OwnerService ownerService;
+
+    @InjectMocks
+    private OwnerController controller;
+
+    private Set<Owner> owners;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        owners = new HashSet<>();
+        owners.add(Owner.builder().id(1L).build());
+        owners.add(Owner.builder().id(2L).build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
+    }
+
+    @Test
+    @Disabled
+    void listOwners() {
+        when(ownerService.findAll()).thenReturn(owners);
+/*
+        mockMvc.perform(get(/)
+            .andExpect(status())*/
+    }
+
+    @Test
+    void find() {
+    }
+}
